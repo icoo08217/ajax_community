9b344857c82cbfcb94c28de509c9acc2c5f120a9
icoo08217 <icoo08216@gmail.com>
2022-07-28 11:23:21 +0900

    json -> Object화

--- src/main/java/com/ll/exam/util/Ut.java
+++ src/main/java/com/ll/exam/util/Ut.java
@@ -2,14 +2,28 @@ package com.ll.exam.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ll.exam.article.dto.ArticleDto;
 
 public class Ut {
+
+    private static final ObjectMapper om;
+
+    static {
+        om = new ObjectMapper();
+    }
     public static class json{
         public static String toStr(Object obj , String defaultValue){
-            ObjectMapper om = new ObjectMapper();
 
             try {
                 return om.writeValueAsString(obj);
+            } catch (JsonProcessingException e) {
+                return defaultValue;
+            }
+        }
+
+        public static Object toObj(String jsonStr, Class<ArticleDto> cls, Object defaultValue ) {
+            try {
+                return om.readValue(jsonStr, cls);
             } catch (JsonProcessingException e) {
                 return defaultValue;
             }
